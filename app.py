1f1bfb1464845cd4e4322afc21919838d665f31f
ArTwoBeni <52721609+ArTwoBeni@users.noreply.github.com>
2026-06-12 13:19:47 -0300

    Update app.py
    senha joao

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if not st.session_state['logged_in']:
         # --- DICIONÁRIO DE USUÁRIOS E SENHAS ---
         usuarios_pcp = {
             "denis.pcp": "Davi&Heitor",
-            "joao.pcp": "46993061"
+            "joao.pcp": "46993062"
         }
 
         usuarios_vendas = {
